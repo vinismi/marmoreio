a5d1a0b8bfc907f16fdebe5ff1f22328a1833701
frizer congelado <frizerzin@gmail.com>
2025-10-22 03:34:05 +0000

    nao deixe notificação no meio no celular, deixe ela do lado pro celular

--- src/components/social-proof-toast.tsx
+++ src/components/social-proof-toast.tsx
@@ -26,8 +26,8 @@ const positions = [
   'bottom-5 right-5',
   'top-5 left-5',
   'top-5 right-5',
-  'top-1/2 -translate-y-1/2 left-5',
-  'top-1/2 -translate-y-1/2 right-5',
+  'top-5 left-1/2 -translate-x-1/2',
+  'bottom-5 left-1/2 -translate-x-1/2',
 ];
 
 
